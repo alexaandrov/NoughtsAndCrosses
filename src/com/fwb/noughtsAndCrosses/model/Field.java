8549266c831c258bee3446116b0f7e05a8f85add
Greg HP <alexaandrovp@gmail.com>
2015-07-07 10:25:17 +0300

    Refactoring Field.java

--- src/com/fwb/noughtsAndCrosses/model/Field.java
+++ src/com/fwb/noughtsAndCrosses/model/Field.java
@@ -4,36 +4,44 @@ public class Field {
 
     private static char field[][] = new char[3][3];
 
-    private int l = 0;
+    private static final int MAX_FIELD_SIZE = 3;
+
+    public static final int MIN_FIELD_SIZE = 0;
+
+    private static int iterator = 0;
+
+    private static int numberOneInTheChar = 49;
+
 
     public Field() {
         createField();
     }
 
     private void createField() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                field[i][j] = (char) (49 + l);
-                l++;
+        for (int lineNumber = MIN_FIELD_SIZE; lineNumber < MAX_FIELD_SIZE; lineNumber++) {
+            for (int cellNumber = 0; cellNumber < MAX_FIELD_SIZE; cellNumber++) {
+                field[lineNumber][cellNumber] = (char) (numberOneInTheChar + iterator);
+                iterator++;
             }
         }
     }
 
     public void showField() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print("[" + field[i][j] + "]");
+        for (int lineNumber = MIN_FIELD_SIZE; lineNumber < MAX_FIELD_SIZE; lineNumber++) {
+            for (int cellNumber = 0; cellNumber < MAX_FIELD_SIZE; cellNumber++) {
+                System.out.print("[" + field[lineNumber][cellNumber] + "]");
             }
             System.out.println();
         }
     }
 
     public void putValue(char value, char symbol) {
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++) {
-                if (field[i][j] == value) {
-                    field[i][j] = symbol;
+        for (int lineNumber = MIN_FIELD_SIZE; lineNumber < MAX_FIELD_SIZE; lineNumber++) {
+            for (int cellNumber = 0; cellNumber < MAX_FIELD_SIZE; cellNumber++) {
+                if (field[lineNumber][cellNumber] == value) {
+                    field[lineNumber][cellNumber] = symbol;
                 }
             }
+        }
     }
 }
